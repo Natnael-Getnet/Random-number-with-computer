800154435247d877ef4e88223c2900b90d99d15f
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2022-10-21 09:32:44 +0300

    Improve random number game

--- main.py
+++ main.py
@@ -10,17 +10,14 @@ def guess_game(high):
     print(rand_number)
     is_over = False
     while not is_over:
-        guess = int(input(f'Guess a number between 1 and {high}'))
+        guess = int(input(f'Guess a number between 1 and {high}\n'))
         if rand_number < guess:
-            print(f'Sorry, guess again. Too high')
+            print(f'Sorry, guess again. Too high\n')
         elif rand_number > guess:
-            print(f'Sorry, guess again. Too Low')
+            print(f'Sorry, guess again. Too Low\n')
         else:
-            print(f'Yay, you have guessed the number {rand_number} correctly!')
+            print(f'Yay, you have guessed the number {rand_number} correctly!\n')
             is_over = True
-
-
-guess_game(1000)
 
 
 def guess_the_number(x):
@@ -33,12 +30,23 @@ def guess_the_number(x):
             guess = random.randint(low, high)
         else:
             guess = low  # Could also be high because high = low
-        feedback = input(f'Is {guess} too high (H) or too low (L) or correct (C) ')
+        feedback = input(f'Is {guess} too high (H) or too low (L) or correct (C)\n')
         if feedback.lower() == 'h':
             high = guess - 1
         elif feedback.lower() == 'l':
             low = guess + 1
-    print(f'Yay, you have guessed the number {guess} correctly!')
+    print(f'Yay, you have guessed the number {guess} correctly!\n')
 
 
-guess_the_number(100)
+def play_game():
+    play = input('Hello there, Do you want to play random number guessing game? Y or N\n')
+    while play.lower() == 'y':
+        strategy = input('Hello there, Do you want to guess (M) or the computer to guess (C)?\n')
+        if strategy.lower() == 'm':
+            guess_game(1000)
+        elif strategy.lower() == 'c':
+            guess_the_number(1000)
+        play = input('Hello there, Do you want to play random number guessing game? Y or N\n')
+
+
+play_game()
